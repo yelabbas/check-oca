b3c24e0ee07d6240864e204118366458a54362f3
EL ABBASSI CHRAIBI Youssef <Youssef El Abbassi>
2020-11-11 15:17:37 +0100

    remove label parameter

--- check_OCA.py
+++ check_OCA.py
@@ -98,18 +98,14 @@ def check_pr(repo,number):
 
 def main():
     # Check if the number of input arguments is correct
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         raise ValueError('Invalid number of arguments!')
 
     # Get the GitHub token
     token=sys.argv[1]
 
-    # Get the list of valid labels
-    valid_labels=sys.argv[2]
-    print(f'Valid labels are: {valid_labels}')
-
     # Get the PR number
-    pr_number_str=sys.argv[3]
+    pr_number_str=sys.argv[2]
 
     repo_name=get_env_var('GITHUB_REPOSITORY')
     github_ref=get_env_var('GITHUB_REF')
